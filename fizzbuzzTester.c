835f213166470be5a9e314800f3bc4c670eaf750
Fiona Bianchi <fiona@hiklas.co.uk>
2017-09-21 16:38:35 +0100

    Adding a new test

--- fizzbuzzTester.c
+++ fizzbuzzTester.c
@@ -17,12 +17,27 @@ BEGIN_TEST(divided_by_three_returns_fizz)
 }
 END_TEST
 
+BEGIN_TEST(divided_by_five_returns_buzz)
+{
+  char result_buffer[10];
+  int result; // Doesn't matter about initializing, it'll be junk
+
+  result = process_one_number(5, result_buffer);
+
+  ASSERT_TRUE( result == 4, "Process one number results in 4" )
+  ASSERT_STRINGS_EQUAL(result_buffer, "buzz", 10, "Compare strings")
+}
+END_TEST
+
+
+
 
 int main(int argc, char* argv[]) 
 {
   START_TESTING("FizzBuzz")
 
   RUN_TEST(&divided_by_three_returns_fizz, "Divide by three returns fizz");
+  RUN_TEST(&divided_by_five_returns_buzz, "Divide by five returns buzz");
 
   END_TESTING
 }
