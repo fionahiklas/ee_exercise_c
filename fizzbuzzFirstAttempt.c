577ed72d0304d45a62af59417e4d622d2251ecd4
Fiona Bianchi <fiona@hiklas.co.uk>
2017-09-21 21:23:01 +0100

    Completed tests and code for first step - even had to fix a segmentation fault

--- fizzbuzzFirstAttempt.c
+++ fizzbuzzFirstAttempt.c
@@ -54,6 +54,7 @@ int process_one_number(int numberToProcess, char* result_buffer)
 }
 
 
+
 /*
  * Process an array of numbers and populate the buffer with
  * the textual result
@@ -61,8 +62,27 @@ int process_one_number(int numberToProcess, char* result_buffer)
  * The return value of the function is number or characters
  * written to the buffer, or 0 on error.
  */
-int process_many_numbers(int* numbersToProcess[], char* result_buffer)
+int process_many_numbers(int* numbersToProcess, int sizeOfNumbersArray, char* result_buffer)
 {
+    char each_number_result_buffer[10];
+    int each_number_result;
+    bool have_result = false;
 
-    return 0;
+    printf("Size of array: %d\n", sizeOfNumbersArray);
+
+    // This is the main result buffer
+    result_buffer[0] = '\0'; // Need to nul-terminate so result works
+
+    for(int i = 0; i < sizeOfNumbersArray; i++)
+    {
+        each_number_result = process_one_number(numbersToProcess[i], each_number_result_buffer);
+        if( each_number_result > 0 )
+        {
+            if( have_result ) strcat(result_buffer, " ");
+            strcat(result_buffer, each_number_result_buffer);
+            have_result = true;
+        }
+    }
+
+    return strlen(result_buffer);
 }
--- fizzbuzzTester.c
+++ fizzbuzzTester.c
@@ -4,6 +4,9 @@
 #include "fizzbuzz.h"
 #include "testing.h"
 
+const int FULL_TEST_DATA[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+const char* FULL_RESULT_FIZZBUZZ = "1 2 fizz 4 buzz fizz 7 8 fizz buzz 11 fizz 13 14 fizzbuzz 16 17 fizz 19 buzz";
+
 
 BEGIN_TEST(divided_by_three_returns_fizz)
 {
@@ -66,6 +69,19 @@ BEGIN_TEST(zero_doesnt_break_mod_operator_use)
 }
 END_TEST
 
+BEGIN_TEST(process_array_of_numbers)
+{
+  char result_buffer[1024]; // Paranoid, made the buffer huge
+  int result; // Doesn't matter about initializing, it'll be junk
+
+  result = process_many_numbers(FULL_TEST_DATA, sizeof(FULL_TEST_DATA)/sizeof(int), result_buffer);
+
+  printf("Result: %d, result string: %s\n", result, result_buffer);
+
+  ASSERT_TRUE( result == strlen(FULL_RESULT_FIZZBUZZ), "Process many numbers results in full string" )
+  ASSERT_STRINGS_EQUAL(result_buffer, FULL_RESULT_FIZZBUZZ, 1024, "Compare strings")
+}
+END_TEST
 
 
 int main(int argc, char* argv[]) 
@@ -77,6 +93,7 @@ int main(int argc, char* argv[])
   RUN_TEST(&divided_by_three_and_five_returns_fizzbuzz, "Divide by three and five returns fizzbuzz");
   RUN_TEST(&a_number_that_doesnt_divide_returns_number, "Return number for something that doesn't divide");
   RUN_TEST(&zero_doesnt_break_mod_operator_use, "Zero shouldn't result in fizzbuzz");
+  RUN_TEST(&process_array_of_numbers, "Test full fizzbuzz results");
 
   END_TESTING
 }
